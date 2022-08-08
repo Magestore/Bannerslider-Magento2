02eb7599c9e9ccdbadf83e19a1ab04dedaf7f9fa
aimsjigar <100894075+aimsjigar@users.noreply.github.com>
2022-08-08 19:20:33 +0530

    give error https://i.imgur.com/jiaXicF.png
    https://i.imgur.com/jiaXicF.png

--- Model/ResourceModel/Report/Collection.php
+++ Model/ResourceModel/Report/Collection.php
@@ -78,7 +78,7 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
      * @param \Psr\Log\LoggerInterface                                     $logger
      * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
      * @param \Magento\Framework\Event\ManagerInterface                    $eventManager
-     * @param \Zend_Db_Adapter_Abstract                                    $connection
+   
      * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb              $resource
      */
     public function __construct(
@@ -88,7 +88,7 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
         \Magento\Framework\Event\ManagerInterface $eventManager,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Framework\Stdlib\DateTime\Timezone $stdTimezone,
-        $connection = null,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
         \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
     ) {
         parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
